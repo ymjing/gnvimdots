bb99cbe8972b5fbc111af9d43a07e2827896f3f9
Yiming Jing <yjing@apache.org>
2022-11-24 18:14:27 -0800

    limit treesitter language parsers

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -4,19 +4,38 @@ function config.nvim_treesitter()
   vim.api.nvim_command('set foldmethod=expr')
   vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
   require('nvim-treesitter.configs').setup({
-    ensure_installed = 'all',
+    ensure_installed = {
+      "bash",
+      "fish",
+      "c",
+      "cpp",
+      "java",
+      "kotlin",
+      "proto",
+      "lua",
+      "go",
+      "json",
+      "yaml",
+      "toml",
+      "make",
+      "python",
+      "rust",
+      "html",
+    },
     ignore_install = { 'phpdoc' },
     highlight = {
       enable = true,
+      disable = { "vim" },
+      additional_vim_regex_highlighting = false,
     },
     textobjects = {
       select = {
         enable = true,
         keymaps = {
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
+          ['fo'] = '@function.outer',
+          ['fi'] = '@function.inner',
+          ['co'] = '@class.outer',
+          ['ci'] = '@class.inner',
         },
       },
     },
